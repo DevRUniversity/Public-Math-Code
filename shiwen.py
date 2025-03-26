d0bb94c2279c26fc7833a3e56d09185da2141f73
DevRUniversity <91238387+DevRUniversity@users.noreply.github.com>
2025-03-26 10:18:45 -0400

    Update shiwen.py

--- shiwen.py
+++ shiwen.py
@@ -22,15 +22,24 @@ def c_n(r,d,n):
 def claimed_function(a_1, a_0,n):
     return (c_n(3,2,n)*a_1 - c_n(3,3,n)*a_0) / (2**(n-1))
 
+def claimed2_function(a_1,a_0,n): # a bit more computationally efficient
+    return ((2 - 1/(2**(n-1))) * a_1) - ((1 - 1/(2**(n-1))) * a_0)
+
 n = 2
 valid_results = True
-while ( n < 1000 and valid_results):
+while ( n < 55 and valid_results):
     result_list.append(normal_sequence(result_list))
-    r = claimed_function(a_1,a_0,n)
-    if(sequence_checker(result_list) == False or r != result_list[len(result_list)-1]):
+    r_1 = claimed_function(a_1,a_0,n)
+    r_2 = claimed2_function(a_1,a_0,n)
+    print(n,r_2, r_1, result_list[len(result_list)-1])
+    if(sequence_checker(result_list) == False or r_1 != result_list[len(result_list)-1] or r_2 != result_list[len(result_list)-1]):
         valid_results = False
-        print(n)
+        print("Failed!")
     n = n + 1
-    
-#fails at n = 54, when computational precision fails
+
+
+if(valid_results == True):
+    print("Cleared all!")
+
+#claimed fails at n = 54, when computational precision fails, so made claimed_2 to try and compute more cleanly but this also fails at n=53 with computation precision fails
 
