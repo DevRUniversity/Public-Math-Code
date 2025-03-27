4cc70f46569142d00ec3b9f4f758300ad13bda4a
DevRUniversity <91238387+DevRUniversity@users.noreply.github.com>
2025-03-27 14:49:01 -0400

    Update shiwen.py

--- shiwen.py
+++ shiwen.py
@@ -25,14 +25,19 @@ def claimed_function(a_1, a_0,n):
 def claimed2_function(a_1,a_0,n): # a bit more computationally efficient
     return ((2 - 1/(2**(n-1))) * a_1) - ((1 - 1/(2**(n-1))) * a_0)
 
+def claimed3_function(a_1,a_0,n): # a bit more computationally efficient
+    return (2*a_1 - a_0) + (a_0 - a_1)/(2**(n-1))
+
 n = 2
 valid_results = True
+print(claimed3_function(a_1,a_0,n))
 while ( n < 55 and valid_results):
     result_list.append(normal_sequence(result_list))
     r_1 = claimed_function(a_1,a_0,n)
     r_2 = claimed2_function(a_1,a_0,n)
-    print(n,r_2, r_1, result_list[len(result_list)-1])
-    if(sequence_checker(result_list) == False or r_1 != result_list[len(result_list)-1] or r_2 != result_list[len(result_list)-1]):
+    r_3 = claimed3_function(a_1,a_0,n)
+    print(n,r_2, r_1, r_3, result_list[len(result_list)-1])
+    if(sequence_checker(result_list) == False or r_3 != result_list[len(result_list)-1]):
         valid_results = False
         print("Failed!")
     n = n + 1
@@ -40,6 +45,10 @@ while ( n < 55 and valid_results):
 
 if(valid_results == True):
     print("Cleared all!")
+
+#claimed fails at n = 54, when computational precision fails, so made claimed_2 to try and compute more cleanly but this also fails at n=53 with computation precision fails
+
+
 
 #claimed fails at n = 54, when computational precision fails, so made claimed_2 to try and compute more cleanly but this also fails at n=53 with computation precision fails
 
